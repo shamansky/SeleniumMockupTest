e009784d6904464339476e5b97b0cebca7a9130d
George Ciurescu <shubst3r@gmail.com>
2020-11-02 11:17:12 +0000

    added more tests for browsing through the app

--- CreditCards.UITests/CreditCardWebAppShould.cs
+++ CreditCards.UITests/CreditCardWebAppShould.cs
@@ -7,7 +7,6 @@ namespace CreditCards.UITests
 {
     public class CreditCardWebAppShould
     {
-
         private const string HomeUrl = "http://localhost:5258/";
         private const string HomeTitle = "Home Page - Credit Cards";
         private const string AboutUrl = "http://localhost:5258/Home/About";
@@ -51,6 +50,9 @@ namespace CreditCards.UITests
             using (IWebDriver driver = new ChromeDriver())
             {
                 driver.Navigate().GoToUrl(HomeUrl);
+                IWebElement generationTokenElement = driver.FindElement(By.Id("GenerationToken"));
+                string initialToken = generationTokenElement.Text;
+
                 DemoHelper.Pause();
                 driver.Navigate().GoToUrl(AboutUrl);
                 DemoHelper.Pause();
@@ -61,7 +63,8 @@ namespace CreditCards.UITests
                 Assert.Equal(HomeTitle, driver.Title);
                 Assert.Equal(HomeUrl, driver.Url);
 
-                //
+                string reloadedToken = driver.FindElement(By.Id("GenerationToken")).Text;
+                Assert.NotEqual(initialToken, reloadedToken);
             }
         }
 
@@ -74,6 +77,8 @@ namespace CreditCards.UITests
                 driver.Navigate().GoToUrl(AboutUrl);
                 DemoHelper.Pause();
                 driver.Navigate().GoToUrl(HomeUrl);
+                IWebElement generationTokenElement = driver.FindElement(By.Id("GenerationToken"));
+                string initialToken = generationTokenElement.Text;
                 DemoHelper.Pause();
                 driver.Navigate().Back();
                 DemoHelper.Pause();
@@ -84,9 +89,24 @@ namespace CreditCards.UITests
 
                 Assert.Equal(HomeTitle, driver.Title);
                 Assert.Equal(HomeUrl, driver.Url);
-                //
+                string reloadedToken = driver.FindElement(By.Id("GenerationToken")).Text;
+                Assert.NotEqual(initialToken, reloadedToken);
             }
         }
 
+        [Fact]
+        public void DisplayProductAndRates()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl(HomeUrl);
+                DemoHelper.Pause();
+
+                IWebElement firstTableCell = driver.FindElement(By.TagName("td"));
+                string firstProduct = firstTableCell.Text;
+
+                Assert.Equal("Easy Credit Card", firstProduct);
+            }
+        }
     }
 }
