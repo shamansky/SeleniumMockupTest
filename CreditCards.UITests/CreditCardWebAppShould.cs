20c590f62db80f9e37d670e8e373801167c6b25b
George Ciurescu <shubst3r@gmail.com>
2020-11-16 22:10:38 +0000

    added tests for cookies and popups

--- CreditCards.UITests/CreditCardWebAppShould.cs
+++ CreditCards.UITests/CreditCardWebAppShould.cs
@@ -3,6 +3,11 @@ using Xunit;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System.Collections.ObjectModel;
+using OpenQA.Selenium.Support.UI;
+using ApprovalTests.Reporters;
+using ApprovalTests.Reporters.Windows;
+using System.IO;
+using ApprovalTests;
 
 namespace CreditCards.UITests
 {
@@ -133,26 +138,114 @@ namespace CreditCards.UITests
         //    }
         //}
 
+        //[Fact]
+        //public void OpenContactFooterLinkInNewTab()
+        //{
+        //    using (IWebDriver driver = new ChromeDriver())
+        //    {
+        //        driver.Navigate().GoToUrl(HomeUrl);
+
+        //        driver.FindElement(By.Id("ContactFooter")).Click();
+
+        //        DemoHelper.Pause();
+
+        //        ReadOnlyCollection<string> allTabs = driver.WindowHandles;
+        //        string homePageTab = allTabs[0];
+        //        string contactTab = allTabs[1];
+
+        //        driver.SwitchTo().Window(contactTab);
+
+        //        DemoHelper.Pause();
+
+        //        Assert.EndsWith("/Home/Contact", driver.Url);
+        //    }
+        //}
+
+        //[Fact]
+        //public void AlertIfLiveChatClosed()
+        //{
+        //    using (IWebDriver driver = new ChromeDriver())
+        //    {
+        //        driver.Navigate().GoToUrl(HomeUrl);
+
+        //        driver.FindElement(By.Id("LiveChat")).Click();
+
+        //        WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+
+        //        IAlert alert = wait.Until(ExpectedConditions.AlertIsPresent());
+
+        //        Assert.Equal("Live chat is currently closed.", alert.Text);
+
+        //        DemoHelper.Pause();
+
+        //        alert.Accept();
+
+        //        DemoHelper.Pause();
+        //    }
+        //}
+
+        //[Fact]
+        //public void NotNavigateToAboutUsWhenCancelClicked()
+        //{
+        //    using (IWebDriver driver = new ChromeDriver())
+        //    {
+        //        driver.Navigate().GoToUrl(HomeUrl);
+        //        Assert.Equal(HomeTitle, driver.Title);
+
+        //        driver.FindElement(By.Id("LearnAboutUs")).Click();
+
+        //        DemoHelper.Pause();
+
+        //        WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+        //        IAlert alertBox = wait.Until(ExpectedConditions.AlertIsPresent());
+
+        //        alertBox.Dismiss();
+
+        //        Assert.Equal(HomeTitle, driver.Title);
+        //    }
+        //}
+
+        //[Fact]
+        //public void NotDisplayCookieUseMessage()
+        //{
+        //    using (IWebDriver driver = new ChromeDriver())
+        //    {
+        //        driver.Navigate().GoToUrl(HomeUrl);
+
+        //        driver.Manage().Cookies.AddCookie(new Cookie("acceptedCookies", "true"));
+
+        //        driver.Navigate().Refresh();
+
+        //        ReadOnlyCollection<IWebElement> message =
+        //            driver.FindElements(By.Id("CookiesBeingUsed"));
+
+        //        Assert.Empty(message);
+
+        //        Cookie cookieValue = driver.Manage().Cookies.GetCookieNamed("acceptedCookies");
+        //        Assert.Equal("true", cookieValue.Value);
+
+        //        driver.Manage().Cookies.DeleteCookieNamed("acceptedCookies");
+        //        driver.Navigate().Refresh();
+        //        Assert.NotNull(driver.FindElement(By.Id("CookiesBeingUsed")));
+        //    }
+        //}
+
         [Fact]
-        public void OpenContactFooterLinkInNewTab()
+        [UseReporter(typeof(BeyondCompareReporter))]
+        public void RenderAboutPage()
         {
             using (IWebDriver driver = new ChromeDriver())
             {
-                driver.Navigate().GoToUrl(HomeUrl);
+                driver.Navigate().GoToUrl(AboutUrl);
 
-                driver.FindElement(By.Id("ContactFooter")).Click();
+                ITakesScreenshot screenShotDriver = (ITakesScreenshot)driver;
 
-                DemoHelper.Pause();
+                Screenshot screenshot = screenShotDriver.GetScreenshot();
 
-                ReadOnlyCollection<string> allTabs = driver.WindowHandles;
-                string homePageTab = allTabs[0];
-                string contactTab = allTabs[1];
+                screenshot.SaveAsFile("aboutpage.bmp", ScreenshotImageFormat.Bmp);
 
-                driver.SwitchTo().Window(contactTab);
-
-                DemoHelper.Pause();
-
-                Assert.EndsWith("/Home/Contact", driver.Url);
+                FileInfo file = new FileInfo("aboutpage.bmp");
+                Approvals.Verify(file);
             }
         }
     }
